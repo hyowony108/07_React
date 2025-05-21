14cde2cdcadc72e455ed63ba5cae01a6b25abb8b
hyowony108 <hyowony108@gmail.com>
2025-05-21 10:54:12 +0900

    jsx 예제

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Exam1 from "./components/Exam1";
 import Exam2 from "./components/Exam2";
+import Exam3 from "./components/Exam3";
+import Exam4 from "./components/Exam4";
+import Exam5 from "./components/Exam5";
 
 function App() {
   // 상태(state)
@@ -19,11 +22,15 @@ function App() {
     </>
     */
 
-    <>
-      {/* showExam 이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링함 */}
-      <button onClick={() => setShowExam(!showExam)}>클릭</button>
-      {showExam && <Exam2 hyowon="hello" test="world" />}
-    </>
+    // <>
+    //   {/* showExam 이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링함 */}
+    //   <button onClick={() => setShowExam(!showExam)}>클릭</button>
+    //   {showExam && <Exam2 hyowon="hello" test="world" />}
+    // </>
+
+    // <Exam3 />
+    // <Exam4 />
+    <Exam5 />
   );
 }
 
